b61b6436fc1c2778bd10cf81af083fa6b4ee243d
Halyna Kotiuk <162026407+halyn41k@users.noreply.github.com>
2024-06-11 21:21:43 +0300

    Update registration, Room List and fix problems

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: [
+      'plugin:vue/vue3-essential',
+      'eslint:recommended',
+      '@vue/prettier'
+    ],
+    parserOptions: {
+      parser: 'babel-eslint',
+    },
+    rules: {
+      // Додайте свої правила тут
+    },
+  };
+  
